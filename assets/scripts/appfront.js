85559784fcfd977d06a4fef208e84955c6c9846f
sfarooq26 <saad.farooq2693@gmail.com>
2022-06-02 21:19:47 +0500

    parseInt

--- assets/scripts/appfront.js
+++ assets/scripts/appfront.js
@@ -3,15 +3,42 @@ alert('Under Development!');
 const defaultResult = 0;
 
 let currentResult = defaultResult;
-
-function add() {
-    currentResult = currentResult + parseInt(userInput.value) ;
-    outputResult(currentResult, '');
+function UserInputNumber() {
+    return parseInt(userInput.value);
 }
 
+function add() {
+    const enteredNumber = UserInputNumber();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber ;
+    outputResult(currentResult, calcDescription);
+}
+function subtract() {
+    const EnteredNumber = UserInputNumber();
+    let calcDescription = `${currentResult} - ${EnteredNumber}`
+    currentResult = currentResult -  EnteredNumber ;
+    outputResult(currentResult, calcDescription);
+}
+
+function multiply() {
+    const EnteredNumber = UserInputNumber();
+    let calcDescription = `${currentResult} * ${EnteredNumber}`
+    currentResult = currentResult *  EnteredNumber ;
+    outputResult(currentResult, calcDescription);
+}
+
+function Division() {
+    const EnteredNumber = UserInputNumber();
+    let calcDescription = `${currentResult} / ${EnteredNumber}`
+    currentResult = currentResult /  EnteredNumber ;
+    outputResult(currentResult, calcDescription);
+}
 addBtn.addEventListener('click', add );
 
+subtractBtn.addEventListener('click', subtract );
+
+multiplyBtn.addEventListener('click', multiply );
+
+divideBtn.addEventListener('click', Division );
 
 
-
- 
