c1d729f36cc26fef2094cbbe0699e3143ec00964
sfarooq26 <saad.farooq2693@gmail.com>
2022-06-04 02:35:20 +0500

    Array & Object

--- assets/scripts/appfront.js
+++ assets/scripts/appfront.js
@@ -3,11 +3,17 @@ alert('Under Development!');
 const defaultResult = 0;
 
 let currentResult = defaultResult;
+
+let logUserEntries = [];
+
 function UserInputNumber() {
     return parseInt(userInput.value);
 }
 /* Can also use ++, --,
-if want to increment/decriment by 1 */
+if want to increment/decriment by 1
+Note: if ++currentResult >> it will get value after the change (of 1)
+if currentResult++ >> it'll get value before the change (of 1)
+*/
 function calcLog(operator, resultBeforeCalc, inputNumber) {
     const calcDescription = `${resultBeforeCalc} ${operator} ${inputNumber}`;
     outputResult(currentResult, calcDescription);
@@ -17,6 +23,14 @@ function add() {
     const initialResult = currentResult;
     currentResult += enteredNumber ;    //simplified operator
     calcLog ('+',initialResult, enteredNumber);
+    const logCalc = {
+        operation: 'ADD',
+        defaultValue: initialResult,
+        userInputValue: enteredNumber,
+        result: currentResult
+    }
+    logUserEntries.push(logCalc);
+    console.log(logUserEntries);
 }
 function subtract() {
     const enteredNumber = UserInputNumber();
